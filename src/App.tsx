5c73daa72cd893628ab85db1f76cfec659437100
David Nguyen <Davidngy2803@gmail.com>
2025-12-10 00:14:06 +0100

    setting up drizzle

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+//import { LoginForm } from './pages/login-form'
+import { SignupForm } from './pages/signup-form'
+
+function App() {
+  return (
+    <>
+      <SignupForm></SignupForm>
+    </>
+  )
+}
+
+export default App
